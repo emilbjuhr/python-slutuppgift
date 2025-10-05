cc88df625248a586604df67bee6e8336a537b6a4
Emil <emil.bjuhr@chasacademy.se>
2025-10-05 20:05:21 +0200

    Jag har skapat de mappar som jag tror jag behöver, samt skrivit på vad som ska ske i programmet

---
+++ main.py
@@ -0,0 +1,12 @@
+# När programmet körs ska en knapp visas som startar själva programmet.
+# När startknappen blir tryckt så kommer en meny som visar följande val:
+#1. Starta övervakningsläge
+#2. Skapa larm
+#3. Konfigurera larm
+#4. Larm lista
+#5. Avsluta programmet
+
+# Starta övervakningsläge
+# Hämta data från def get_cpu_usage(), def get_memory_usage() och def get_disk_usage()
+# Visa data i någon form av UI som uppdateras varje sekund 
+# Skriv ut att användaren kan trycka enter för att återgår den till huvudmenyn
---
+++ monitor.py
@@ -0,0 +1,4 @@
+# Skapa dessa funktioner
+# get_cpu_usage()
+# def get_memory_usage() 
+# def get_disk_usage()
