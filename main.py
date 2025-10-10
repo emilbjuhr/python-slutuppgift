c0c71470f0069d2f53fdd10d3f6ec6c147d6304d
Emil <emil.bjuhr@chasacademy.se>
2025-10-10 09:14:08 +0200

    Put the monitoring functionality in menu 1.

--- main.py
+++ main.py
@@ -11,23 +11,10 @@
 # Visa data i någon form av UI som uppdateras varje sekund 
 # Skriv ut att användaren kan trycka enter för att återgår den till huvudmenyn
 import psutil
+import time
+import msvcrt
 
-print("----")
-psutil.cpu_percent(interval=1,percpu=True)
-print(f"{psutil.cpu_percent()} % CPU-användning")
-print("----")
-psutil.virtual_memory()
-mem = psutil.virtual_memory()
-print(f"{mem.percent} % Minnes-användning")
-print("----")
-psutil.disk_usage("/")
-disk_usage = psutil.disk_usage("/")
-print(f"{disk_usage.percent} % Disk-användning")
-
-
-
-
-'''
+done_monitoring = False
 start = True
 
 while(start):
@@ -37,19 +24,34 @@ while(start):
     print("4: Larm lista")
     print("5: Avsluta program")
 
-    if input("1"):
-        pass
+    menu_input = input("Välj ett alternativ: ")
+
+    if menu_input == "1":
+        while(not done_monitoring):
+            cpu_usage = psutil.cpu_percent(interval=1,percpu=False)
+            print(f"{cpu_usage} % CPU-användning")
+            mem = psutil.virtual_memory()
+            print(f"{mem.percent} % Minnes-användning")
+            disk_usage = psutil.disk_usage("/")
+            print(f"{disk_usage.percent} % Disk-användning")
+            print("Tryck Enter för att avsluta övervakning")
+            print("----")
+            time.sleep(2)
+            if msvcrt.kbhit():
+                key = msvcrt.getch()
+                if key == b'\r':
+                    done_monitoring = True
+
     
-    elif input("2"):
+    elif menu_input == "2":
         pass
 
-    elif input("3"):
+    elif menu_input == "3":
         pass
 
-    elif input("4"):
+    elif menu_input == "4":
         pass
 
-    elif input("5"):
-        start = False
+    elif menu_input == "5":
         print("Avslutar programmet")
-'''
+        start = False
